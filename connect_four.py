15e98f44cab272376e6caf2835445b52a3b8851e
Bymith <168062321+bymith@users.noreply.github.com>
2024-05-02 13:30:01 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -29,10 +29,13 @@ def read_board():
   boardLines.pop(0)
   
   y=0
+  
+  print(len(boardLines))
   for line in boardLines:
     x=0
+    print(len(line))
     for character in line:
-      board[y][x] = character
+      board[x][y] = character # he he wrong side im dumb
       x+=1
     y+=1
 
