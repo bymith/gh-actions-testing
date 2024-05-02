6a9ee725610e267e266b0dcf5b1a464819e968a6
Bymith <168062321+bymith@users.noreply.github.com>
2024-05-02 12:15:51 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -1,8 +1,17 @@
+
+from github import Github
+
 def main():
+  
+  repo = Github(os.environ['GITHUB_TOKEN']).get_repo(os.environ['GITHUB_REPOSITORY'])
+  issue = repo.get_issue(number=int(os.environ['ISSUE_NUMBER']))
   readme_text = """
   # Hello!
   ## This is testing :)
-  """
+
+  
+  """ + issue.title()
+  
   with open("README.md", "w") as f:
         f.write(readme_text)
 
