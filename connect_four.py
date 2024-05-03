89c8715b7eec621dc927af4472f5950b1a38c3bc
Bymith <168062321+bymith@users.noreply.github.com>
2024-05-03 12:10:49 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -111,17 +111,22 @@ def main():
     f.write(readme_text)
 
   with open("boardstate.board", "w") as f:
-    theString = "0"
+    theString = "0\n"
     if isRed:
-      theString = "1"
-    for column in board:
-      print(column)
-      for rowItem in column:
-        print(rowItem)
-        if rowItem == ".":
-          theString += "0"
-        else:
-          theString += rowItem        
+      theString = "1\n"
+
+    theString += """%s%s%s%s%s%s%s
+    %s%s%s%s%s%s%s
+    %s%s%s%s%s%s%s
+    %s%s%s%s%s%s%s
+    %s%s%s%s%s%s%s
+    %s%s%s%s%s%s%s""" % (
+        board[0][0],board[1][0],board[2][0],board[3][0],board[4][0],board[5][0],board[6][0],
+        board[0][1],board[1][1],board[2][1],board[3][1],board[4][1],board[5][1],board[6][1],
+        board[0][2],board[1][2],board[2][2],board[3][2],board[4][2],board[5][2],board[6][2],
+        board[0][3],board[1][3],board[2][3],board[3][3],board[4][3],board[5][3],board[6][3],
+        board[0][4],board[1][4],board[2][4],board[3][4],board[4][4],board[5][4],board[6][4],
+        board[0][5],board[1][5],board[2][5],board[3][5],board[4][5],board[5][5],board[6][5])
     f.write(theString)
   
   issue.edit(state='closed') # we done :)
