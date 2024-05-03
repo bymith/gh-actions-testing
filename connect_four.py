02816e82be75bb19ea8ed8768da7103196d4f206
Bymith <168062321+bymith@users.noreply.github.com>
2024-05-03 11:47:59 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -44,6 +44,22 @@ def read_board():
       x+=1
     y+=1
 
+def fall(colNum):
+  if not board[colNum] == ".":
+    issue.create_comment("bruh that column is filled...")
+    return
+
+  y = 0
+  for rowItem in board[colNum]:
+    if not rowItem == ".":
+      if isRed:
+        board[colNum][y] = "x"
+      else:
+        board[colNum][y] = "o"
+      break
+    y+=1
+    
+
 def main():
 
   if not issue.title.startswith("dropTile|"):
@@ -56,6 +72,8 @@ def main():
 
   if isRed:
     turn = "red"
+
+  fall(int(issue.title.split("|")[1]))
   
   readme_text = """
   # Hello!
@@ -73,13 +91,13 @@ def main():
   | %s | %s | %s | %s | %s | %s | %s |
   
   """ % (turn,
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C1&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C2&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C3&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C4&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C5&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C6&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
-        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7Cred%7C7&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C1&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C2&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C3&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C4&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C5&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C6&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
+        "https://github.com/bymith/gh-actions-testing/issues/new?title=dropTile%7C7&body=Hit+sumbit+new+issue+or+just+press+enter.+Then+just+wait+like+30+seconds+for+it+to+update+%3A%29",
         board[0][0],board[1][0],board[2][0],board[3][0],board[4][0],board[5][0],board[6][0],
         board[0][1],board[1][1],board[2][1],board[3][1],board[4][1],board[5][1],board[6][1],
         board[0][2],board[1][2],board[2][2],board[3][2],board[4][2],board[5][2],board[6][2],
