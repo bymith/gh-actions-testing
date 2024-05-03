34c17ea8dd8efc9c6912f952785e76c6031d8168
Bymith <168062321+bymith@users.noreply.github.com>
2024-05-03 12:11:28 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -127,6 +127,8 @@ def main():
         board[0][3],board[1][3],board[2][3],board[3][3],board[4][3],board[5][3],board[6][3],
         board[0][4],board[1][4],board[2][4],board[3][4],board[4][4],board[5][4],board[6][4],
         board[0][5],board[1][5],board[2][5],board[3][5],board[4][5],board[5][5],board[6][5])
+    print(board)
+    print(theString)
     f.write(theString)
   
   issue.edit(state='closed') # we done :)
